e8c7fb8b95f0a782f3167c5ddce9076f9eb37b44
StefMa <StefMaDev@outlook.com>
2024-04-23 11:18:37 +0200

    Prepare for next version

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ val dokkaJar = tasks.register<Jar>("dokkaJar") {
     archiveClassifier.set("javadoc")
 }
 
-version = "2.2.0"
+version = "2.3.0-SNAPSHOT"
 group = "com.ioki.lokalise"
 publishing {
     publications {
